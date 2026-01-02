f413fec3c3ba4e5e6f5c9f1fbc2f9864c4a930d2
lassyamkotian <lassyalasy@gmail.com>
2026-01-02 17:57:04 +0530

    Remove all files from repository

--- SpecEI_app/specei_app/android/app/src/main/kotlin/com/specei/specei_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.specei.specei_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- SpecEI_app/specei_app/server/run_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing dependencies...
-pip install -r requirements.txt
-echo.
-echo Starting Whisper Server...
-python whisper_server.py
-pause
